47c7b3b5d16c983bfa7f5eb3faef52c89dc7f96f
Ruan-Pablo <ruestacio@gmail.com>
2025-01-01 19:17:40 -0300

    API REST, rotas CRUD

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ cria o package.json -> sumario que fica os scritps dos pacotes, quando tem os co
 
 ## express
 
-é um framework para lidar com a parte de servidor com o node
+é um framework para lidar com a parte de servidor com o node (req, res)
 
 Para instalar: 
 - npm install express
@@ -22,3 +22,33 @@ para instalar(global):
 - npm install -g yarn
 
 É criado a pasta `node modules` q possui varios pacotes, com o express la dentro
+
+## CRUD
+
+Rotas:
+- GET: solicita "read" (podes ser requisitado na URL)
+- POS: envia "create" ( daqui pra baixo so via ajax)
+- PUT: atrualiza
+- DELETE: apaga
+
+### insominia
+
+Ferramenta para mandar os gets.. e ver a resposta dos requests
+
+### nodemon
+atualiza o servidor sem precisar ficar fechando e reiniciando 
+
+para instalar:
+- yarn global add nodemon
+
+e criar um novo script no package.json:
+- em scripts
+- cria um "start":"nodemon ./index.js" - no arquivo inicial
+
+### morgan
+toda vez que alguem acesse ele log com a rota acessada
+
+tipo um retorno do status. (GET / 404)
+
+instalação no package.json:
+- yarn add morgan
--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
-// /teste é a rota
-app.get('/teste', (req, res) => {
-    // regra de negocio
-    console.log('achou')
-    res.json({mensagem: 'Rota funcionando'});
+// dev - ambiente de desenvolvimento: tem a ver com variavel de ambiente do node
+app.use(morgan('dev'));
+
+
+
+app.get('/', (req, res) => {
+    // recuperar todos os registros
+    res.json({mensagem: 'PEGAR TODOS OS REGISTROS'});
 });
+
+app.get('/:id', (req, res) => {
+    // recuperar um registro
+    const id = req.params.id;
+    res.json({mensagem: `PEGAR REGISTRO COM ID: ${id}`});
+});
+
+// CRIAR UM REGISTRO
+app.post('/', (req, res) => {
+    const body = req.body;
+    // na proxima vai ter esse body tratado
+    res.json({mensagem: 'CRIAR REGISTRO'});
+})
+// ATUALIZAR
+app.put('/:id', (req,res) => {
+    const id = req.params.id;
+    res.json({mensagem: `ATUALIZAR O REGISTRO COM ID: ${id}`})
+})
+// APAGAR
+app.delete('/:id', (req, res)=>{
+    const id = req.params.id;
+    res.json({mensagem: `DELETAR O REGISTRO COM ID: ${id}`})
+})
 
 app.listen(3000, () => {
     console.log('Meu servidor esta funfando')
